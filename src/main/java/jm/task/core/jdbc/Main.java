8c145a9d8053314361027b7e604e1aa53733583d
IgorVL <ivgtrk@hotmail.com>
2022-08-23 23:47:46 +0300

    New commit: Создал и проверил соединение с БД.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util u = new Util();
+        u.OpenConnection("jdbc:mysql://localhost:3306/users", "root", "root");
+        u.CloseConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,42 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    Connection connection;
+    Driver driver;
+
+    public void OpenConnection(String url, String username, String password) {
+        try {
+            driver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException ex) {
+            System.out.println("Error create or registered driver!");
+            return;
+        }
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            if(!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error create connection!");
+        }
+    }
+
+    public void CloseConnection() {
+        try {
+            if (!connection.isClosed()) {
+                connection.close();
+                System.out.println("Соединение с БД закрыто!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Cannot close connection!");
+        }
+    }
 }
